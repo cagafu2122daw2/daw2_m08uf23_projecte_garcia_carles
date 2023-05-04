5dbdfa934f4bb86d6a85b7fe8754be2e06a03057
cagafu <cagafu@zend-cagafu.fjeclot.net>
2023-05-04 20:49:33 +0200

    Primer commit del projecte

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+    	<title>Inici</title>
+	</head>
+	<body>
+		<h1>PROJECTE DE GESTIÓ DE BASE DE DADES</h1>
+		<h2>PROJECTE DAW2 M08UF2 M08UF3 </h2>
+		<a href="http://zend-cagafu.fjeclot.net/autent/login.php">Inicia sessió</a>
+	</body>
+</html>
